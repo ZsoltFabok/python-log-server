e715b004c07b82116753df29f128db3794612854
Zsolt Fabok <me@zsoltfabok.com>
2017-06-24 22:52:09 +0200

    write log message to stdout

--- log_server/server.py
+++ log_server/server.py
@@ -1,15 +1,21 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
 import yaml
+import collections
 
 
 class Server():
     class MyHandler(BaseHTTPRequestHandler):
+        # FIXME: server should log to somewhere else, not to stdout
         def do_POST(self):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             content_len = int(self.headers.get('content-length', 0))
-            print(self.rfile.read(content_len))
+            log_entry = log_entry_parser(self.rfile.read(content_len))
+            # FIXME this should go to a file
+            print(' - '.join(log_entry.values()))
+
             return
 
     def start(self, config_file):
@@ -27,5 +33,13 @@ class Server():
             print(e)
 
 
-def log_entry_parser(log_entry):
-    return log_entry
+def log_entry_parser(log_entry_raw):
+    log_entry = parse.unquote(log_entry_raw.decode())
+    log_data = dict([(x.split('=')[0],x.split('=')[1]) for x in log_entry.split('&')])
+
+    return collections.OrderedDict({
+        'datetime':    log_data['asctime'].replace('+', ' '),
+        'logger_name': log_data['name'],
+        'level_name':  log_data['levelname'],
+        'message':     log_data['message'].replace('+', ' ')
+    })
--- tests/test_log_entry_parser.py
+++ tests/test_log_entry_parser.py
@@ -1,4 +1,5 @@
 import unittest
+from urllib import parse
 from log_server.server import log_entry_parser
 
 
@@ -6,4 +7,7 @@ class LogEntryParserTestCase(unittest.TestCase):
 
     def test_parser(self):
         log_entry = b'pathname=C%3A%2FUsers%2Fzsolt%2FWork%2Fpython-log-server%2Flog_client.py&funcName=%3Cmodule%3E&relativeCreated=24.088144302368164&thread=13352&exc_info=None&name=localhost&msg=hello+zsolti&asctime=2017-06-23+22%3A33%3A43%2C988&stack_info=None&threadName=MainThread&exc_text=None&levelname=INFO&process=16968&args=%28%29&created=1498250023.988958&levelno=20&processName=MainProcess&lineno=24&module=log_client&message=hello+zsolti&msecs=988.9578819274902&filename=log_client.py'
-        self.assertEqual(None, log_entry_parser(log_entry))
+        parsed_log_entry = log_entry_parser(log_entry)
+
+        expected = {'logger_name': 'localhost', 'level_name': 'INFO', 'message': 'hello zsolti', 'datetime': '2017-06-23 22:33:43,988'}
+        self.assertEqual(expected, parsed_log_entry)
